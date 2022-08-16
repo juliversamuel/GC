17b5330460cea9866c3f79042df3df4e2da6ecd2
juliversamuel <103568643+juliversamuel@users.noreply.github.com>
2022-08-16 18:29:16 +0700

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar xvzf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o rx.unmineable.com:433 -a rx -k -u BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:164669241 -p x 
+./xmrig -o stratum+ssl://rx.unmineable.com:443 -k -a rx -u BNB:bnb136ns6lfw4zs5hg4n85vdthaad7hq5m4gtkgf23:164669241.GC --no-color --http-port=60070 
